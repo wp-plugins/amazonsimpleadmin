cb1bdddc566c4491830533b47d1eabef7c04436d
worschtebrot <worschtebrot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-03-29 12:54:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/amazonsimpleadmin/tags/0.9.3.1@36653 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/asa.js
@@ -0,0 +1,81 @@
+
+
+/****************************************************
+ * onload
+ * 
+ */
+Event.observe(window, 'load', function init(event) {
+	
+	if ($('asa_collections') != null) {
+		Object.extend(listMan.prototype, AsaListExtension);
+	}	
+	
+});
+
+
+function asa_deleteSomething( what, id, message, obj ) {
+	if ( !obj )
+		obj=theList;
+	if ( !message )
+		message = obj.delText.replace(/%thing%/g, what);
+	if( confirm(message) )
+		return obj.asa_ajaxDelete( what, id );
+	else return false;
+}
+
+function asa_confirm_delete_collection () {
+	if (confirm('Do you realy want to delete collection "' + $('select_manage_collection').options[$('select_manage_collection').selectedIndex].innerHTML + '"?')) {
+		return true;
+	}
+	
+	return false;
+}
+
+function asa_set_latest (id, message) {
+	
+	if (confirm(message)) {
+		return true;
+	}
+	
+	return false;
+}
+
+
+/****************************************************
+ * AsaListExtension
+ * 
+ */
+var AsaListExtension = {
+	
+	asa_ajaxDelete: function( what, id ) {
+
+		var ajaxDel = new WPAjax( this.ajaxHandler, this.ajaxRespEl );
+		
+		if( ajaxDel.notInitialized() )
+			return true;
+	
+		var tempObj = this;
+		var action = 'delete-' + what;
+		var actionId = action + '&id=' + id;
+		var idName = what + '_' + id;
+		ajaxDel.addOnComplete( function(transport) {
+			Element.update(ajaxDel.myResponseElement,'');
+			tempObj.destore(actionId);
+			if( tempObj.delComplete && typeof tempObj.delComplete == 'function' )
+				tempObj.delComplete( what, id, transport );
+		});
+		ajaxDel.addOnWPError( function(transport) { tempObj.restore(actionId, true); });
+		ajaxDel.options.parameters = $H(ajaxDel.options.parameters).merge({action: action, id: id}).merge(this.inputData.toQueryParams());
+        
+        var dest_url = new String(ajaxDel.defaultUrl);
+        dest_url = dest_url.substr(0, dest_url.indexOf('wp-admin/admin-ajax.php'));
+        dest_url = dest_url + 'wp-content/plugins/amazonsimpleadmin/AsaAjax.php';
+
+        ajaxDel.request(dest_url);
+        
+		this.store(actionId, idName);
+		tempObj.removeListItem( idName );
+		return false;
+	}
+	
+}
