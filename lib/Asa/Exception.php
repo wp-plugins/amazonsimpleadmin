a289fada66893b4ee5ddee9186c16c4cd3aae19b
worschtebrot <worschtebrot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-18 23:15:14 +0000

    * Add: New Asa classes
    git-svn-id: https://plugins.svn.wordpress.org/amazonsimpleadmin/trunk@452667 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/Asa/Exception.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * AmazonSimpleAdmin - Wordpress Plugin
+ * 
+ * @author Timo Reith
+ * @copyright Copyright (c) 2007-2011 Timo Reith (http://www.wp-amazon-plugin.com)
+ * 
+ * 
+ */
+
+class Asa_Exception extends Exception {}
+?>
---
+++ lib/Asa/Service/Amazon/Request/Socket.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * AmazonSimpleAdmin - Wordpress Plugin
+ * 
+ * @author Timo Reith
+ * @copyright Copyright (c) 2007-2011 Timo Reith (http://www.wp-amazon-plugin.com)
+ * 
+ * 
+ */
+
+require_once 'Asa/Service/Amazon/Request/Abstract.php';
+
+class Asa_Service_Amazon_Request_Socket extends Asa_Service_Amazon_Request_Abstract 
+{
+    
+    public function send(array $url_params)
+    {
+        $this->build($url_params);
+                
+        if (!$request = $this->getRequest()) {
+			return false;
+		}
+		
+		$config = array(
+    		// socket is default, but for clarity
+            'adapter' => 'AsaZend_Http_Client_Adapter_Socket', 
+        );
+            
+        $client = new AsaZend_Http_Client($request, $config);
+
+        $response = $client->request('GET');
+        
+        if ($response->isSuccessful()) {
+            $this->_response = $response->getBody();
+        }
+				
+		return $this->_response;
+    }
+}
+?>
