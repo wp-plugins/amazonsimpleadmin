976d9088029c0db44c97754278eed70d093db373
worschtebrot <worschtebrot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-12 22:42:16 +0000

    Update service classes
    git-svn-id: https://plugins.svn.wordpress.org/amazonsimpleadmin/trunk@624269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/Asa/Service/Amazon/Debug.php
+++ lib/Asa/Service/Amazon/Debug.php
@@ -10,6 +10,7 @@
 
 require_once 'Asa/Service/Amazon/Interface.php';
 require_once 'Asa/Service/Amazon/Exception.php';
+require_once 'Asa/Debugger.php';
 
 class Asa_Service_Amazon_Debug implements Asa_Service_Amazon_Interface
 {
@@ -18,11 +19,26 @@ class Asa_Service_Amazon_Debug implements Asa_Service_Amazon_Interface
      * @var Asa_Service_Amazon
      */
     protected $_asa;
+
+    /**
+     * @var Asa_Debugger
+     */
+    protected $_debugger;
     
-    
+
+    /**
+     * @param $asa
+     */
     function __construct($asa) 
     {
         $this->_asa = $asa;
+
+        try {
+            // init the debugger
+            $this->_debugger = Asa_Debugger::factory();
+        } catch (Exception $e) {
+            
+        }
     }
     
     /**
@@ -32,9 +48,18 @@ class Asa_Service_Amazon_Debug implements Asa_Service_Amazon_Interface
     public function itemLookup($asin, array $options=array()) 
     {
         $result = $this->_asa->itemLookup($asin, $options);
-        
+
+        if ($this->_debugger instanceof Asa_Debugger) {
+            $debug = 'Action: '. PHP_EOL . __METHOD__ . PHP_EOL . PHP_EOL;
+            $debug .= 'ASIN: '. PHP_EOL . $asin . PHP_EOL . PHP_EOL;
+            $debug .= 'Request: '. PHP_EOL . $this->_asa->getRequest()->getRequest() . PHP_EOL . PHP_EOL;
+            $debug .= 'Response: ' . PHP_EOL . $this->_asa->getRequest()->getResponse()->getBody();
+    
+            $this->_debugger->write($debug);
+        }
+
         if ($result != false) {
-            $this->_debugResponse($this->_asa->getRequest()->getResponse());
+            $this->_debugResponse($this->_asa->getRequest()->getResponse()->getBody());
         }
         
         return $result;
@@ -47,12 +72,54 @@ class Asa_Service_Amazon_Debug implements Asa_Service_Amazon_Interface
     public function itemSearch(array $options)
     {
         $result = $this->_asa->itemSearch($options);
-       
+
+        if ($this->_debugger instanceof Asa_Debugger) {
+            $debug = 'Action: '. PHP_EOL . __METHOD__ . PHP_EOL . PHP_EOL;
+            $debug .= 'Request: '. PHP_EOL . $this->_asa->getRequest()->getRequest() . PHP_EOL . PHP_EOL;
+            $debug .= 'Response: ' . PHP_EOL . $this->_asa->getRequest()->getResponse()->getBody();
+    
+            $this->_debugger->write($debug);
+        }
+        
         if ($result != false) {
-            $this->_debugResponse($this->_asa->getRequest()->getResponse());
+            $this->_debugResponse($this->_asa->getRequest()->getResponse()->getBody());
         }
         
         return $result;
+    }    
+    
+    /**
+     * (non-PHPdoc)
+     * @see Asa_Service_Amazon_Interface::testConnection()
+     */
+    public function testConnection()
+    {
+        $exception = null;
+        
+        try {
+            $result = $this->_asa->itemSearch(array('SearchIndex' => 'Books', 'Keywords' => 'php'));
+        } catch (Exception $exception) {
+            // catch the exception and throw it after writing the debugging information
+        }
+
+        if ($this->_asa->getRequest()->getResponse()->isError() && $this->_debugger instanceof Asa_Debugger) {
+            $debug = 'Action: '. PHP_EOL . 'Test the connection to Amazon API by sending a search request' . PHP_EOL . PHP_EOL;
+            $debug .= 'Request: '. PHP_EOL . $this->_asa->getRequest()->getRequest() . PHP_EOL . PHP_EOL;
+            $debug .= 'Response: ' . PHP_EOL . $this->_asa->getRequest()->getResponse()->getBody();
+    
+            $this->_debugger->write($debug);
+        }
+        
+        if ($result != false) {
+            $this->_debugResponse($this->_asa->getRequest()->getResponse()->getBody());
+        }
+        
+        if ($exception instanceof Exception) {
+            // now throw the catched exception
+            throw $exception;
+        }
+        
+        return $result;       
     }    
     
     
